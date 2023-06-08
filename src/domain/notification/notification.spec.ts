70a8ca29ae8d929ca2d76feba8c6ab4e0f8b7946
Marcos Fiuza <marcos@prooceano.com.br>
2023-06-08 10:14:19 -0300

    feat: improve notification class and introduce notification error class

--- src/domain/notification/notification.spec.ts
+++ src/domain/notification/notification.spec.ts
@@ -32,4 +32,78 @@ describe("Unit test for notification", () => {
 
         expect(notification.messages()).toBe("error message 1, error message 2, error message 3");
     });
+
+    it("should check if notification has at least one error", () => {
+        const notification = new Notification();
+
+        const error = {
+            message: "error message",
+            context: "context"
+        };
+
+        notification.addError(error);
+
+        expect(notification.hasErrors("context")).toBe(true);
+
+        expect(notification.hasErrors()).toBe(true);
+    });
+
+    it("should get all errors props", () => {
+        const notification = new Notification();
+
+        const error1 = {
+            message: "error message 1",
+            context: "context 1"
+        };
+
+        notification.addError(error1);
+
+        expect(notification.getErrors()).toStrictEqual([error1]);
+
+        const error2 = {
+            message: "error message 2",
+            context: "context 2"
+        };
+
+        notification.addError(error2);
+
+        expect(notification.getErrors()).toStrictEqual([error1, error2]);
+
+        expect(notification.getErrors("context 1")).toStrictEqual([error1]);
+
+        expect(notification.getErrors("context 2")).toStrictEqual([error2]);
+    });
+
+    it("should clear errors", () => {
+        const notification = new Notification();
+
+        const error1 = {
+            message: "error message 1",
+            context: "context 1"
+        };
+
+        const error2 = {
+            message: "error message 2",
+            context: "context 2"
+        };
+
+        const error3 = {
+            message: "error message 3",
+            context: "context 3"
+        };
+
+        notification.addError(error1);
+        notification.addError(error2);
+        notification.addError(error3);
+
+        expect(notification.getErrors()).toStrictEqual([error1, error2, error3]);
+
+        notification.clearErrors("context 1");
+
+        expect(notification.getErrors()).toStrictEqual([error2, error3]);
+
+        notification.clearErrors();
+
+        expect(notification.getErrors()).toStrictEqual([]);
+    });
 });
--- src/domain/notification/notification.ts
+++ src/domain/notification/notification.ts
@@ -1,19 +1,35 @@
-export type NotificationError = {
+export type NotificationErrorProps = {
     message: string,
     context: string
 };
 
 export default class Notification {
-    private errors: NotificationError[] = [];
+    protected _errors: NotificationErrorProps[] = [];
 
-    addError(error: NotificationError) {
-        this.errors.push(error);
+    addError(error: NotificationErrorProps) {
+        this._errors.push(error);
     }
 
     messages(context?: string): string {
-        return this.errors
+        return this._errors
             .filter(error => error.context === context || context === undefined)
             .map(error => error.message)
             .join(", ");
+    }
+
+    hasErrors(context?: string): boolean {
+        return this._errors
+            .filter(error => error.context === context || context === undefined)
+            .length > 0;
+    }
+
+    getErrors(context?: string): NotificationErrorProps[] {
+        return this._errors
+            .filter(error => error.context === context || context === undefined);
+    }
+
+    clearErrors(context?: string) {
+        this._errors = this._errors
+            .filter(error => error.context !== context && context !== undefined);
     }
 };
